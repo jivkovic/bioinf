c20688d43b1dc922c4a274c1824a215d9e452810
Josip Ivkovic <josip.ivkovic@fer.hr>
2015-01-14 22:00:21 +0100

    internal added

--- minimizer.py
+++ minimizer.py
@@ -6,7 +6,6 @@ l = w+k-1
 internal_minimizers = []
 end_minimizers = []
 
-
 def internal(str):
   array = []
   
@@ -15,22 +14,3 @@ def internal(str):
     
   array.sort()
   return array[0]
-  
-  
-def end(str):
-  array = []
-  
-  for i in range (0, len(str)-k+1):
-    array.append(str[i:i+k])
-    
-  array.sort()
-  return array[0]
-  
-
-for i in range (0, len(Genome)-l+1):
-  print internal(Genome[i:i+l])
-  
-print "\r-\r"
-for i in range (0, len(Genome)-k+1):
-  print end(Genome[0:i+k])
-
