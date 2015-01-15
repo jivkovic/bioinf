96307410083d86e5c494abbe2385791edf7dbc35
Josip Ivkovic <josip.ivkovic@fer.hr>
2015-01-15 17:36:22 +0100

    input fixed

--- minimizer.py
+++ minimizer.py
@@ -1,8 +1,11 @@
-Genome = "231032101233101"
+#Genome = "231032101233101"
 w = 4
 k = 3
 l = w+k-1
 
+with open ("tests/test10.txt", "r") as myfile:
+    Genome=myfile.read().replace('\n', '').replace('\r', '')
+    
 internal_minimizers = []
 end_minimizers = []
 
@@ -25,8 +28,18 @@ def end(str):
   return array[0]
 
 for i in range (0, len(Genome)-l+1):
-  print internal(Genome[i:i+l])
+  internal_minimizers.append(internal(Genome[i:i+l]))
+  
+#remove duplicates
+internal_minimizers = list(set(internal_minimizers))
+
+f = open('output.txt','w')
+for item in internal_minimizers:
+  f.write("%s\n" % item)
+f.close()
   
 print "\r-\r"
-for i in range (0, len(Genome)-k+1):
-  print end(Genome[0:i+k])
+#for i in range (0, len(Genome)-k+1):
+#  end_minimizers.append(end(Genome[0:i+k]))
+  
+print "done!"
